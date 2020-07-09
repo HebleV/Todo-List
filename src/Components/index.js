e45e76905d5276017a39dfb70b3063c1283ce0b4
HebleV <visheble@yahoo.com>
2020-07-10 01:05:48 +0530

    Refactoring the code

--- src/Components/index.js
+++
@@ -1,3 +0,0 @@
-// export { default as Add } from './Add/Add.js';
-// export { default as RenderList } from './RenderList/RenderList.js';
-// export { default as CountryPicker } from './CountryPicker/CountryPicker';
